f1b3f3f5a646deeb62077efff4e1a0fa18e97ee8
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-04-17 15:32:38 -0500

    starting over

--- script.js
+++ script.js
@@ -1,74 +1,38 @@
-/*----- constants -----*/
-const SHAPES = {
-    '0': 'null',
-    '1': 'X',
-    '-1': 'O'
+// Roadmap:
+// Store all of the elements we need to access as variables in js
+// Access each element of the grid, and be able to handle a click event, change tile adding either x or o based on turn
+// create logic for tie being if all there are no spaces in the array with label 0 call tie
+// create logic for restart button
+// create logic for win conditions
+// create logic for keeping score
 
+
+
+
+
+
+
+/*----- constants -----*/
+const PLAYERS = {
+    '1': {
+        name: 'Player 1',
+        symbol: 'X'
+    },
+    '-1': {
+        name: 'Player 2',
+        symbol: 'O'
+    }
 }
+/*----- cached elements  -----*/
+
+
 
 /*----- state variables -----*/
-let board;
-let turn;
-let winner;
 
-/*----- cached elements  -----*/
-const xWins = document.getElementById("scoreXVal");
-const OWins = document.getElementById("scoreOVal")
-const resetBtn = document.getElementById("restartButton");
-const gameBoard = document.getElementById("gameBoard");
-const gridIndices = [...document.querySelectorAll('#gameBoard > div')]
+
 
 
 /*----- event listeners -----*/
-document.getElementById('gameBoard').addEventListener('click', tileChange)
-resetBtn.addEventListener('click', initialize)
+
 
 /*----- functions -----*/
-initialize()
-function initialize() {
-    board = [
-            [0, 0, 0] //row 1
-            [0, 0, 0] //row 2
-            [0, 0, 0] //row 3
-    //column 1  2  3
-        ]
-        turn = 1;
-        winner = null;
-        render()
-    }
-
-function handleTile(evt) {
-    const colIdx = gridIndices.indexOf(evt.target);
-    //Guards...
-    if (colIdx === -1) return;
-    // shortcut to the column array?
-    const colArray = board[colIdx]
-    //find index of the first 0 in colArray
-    const rowIdx = collArr.indexOf(0)
-    // update the board state with the current player value
-    colArr[rowIdx] = turn;
-    //switch turn player
-    turn *= -1;
-
-}
-
-
-
-function render() {
-    board.forEach(function(rowArr, rowIdx) {
-        //iterates over the cells in rows
-        rowArr.forEach(function(cellValue, colIdx) {
-            const cellId = `r${rowIdx}c${colIdx}`;
-            const cellEl = getElementById(cellId)
-
-      
-          });
-        });
-      }
-      
-function displayMessages() {
-
-}
-function handleControls() {
-
-}
